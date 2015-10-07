38f43a9d4c82a779f062097dd55800774313a1ec
brinden wallace <wallaceb@student.swosu.edu>
2015-10-07 10:47:49 -0500

    do some stuff

--- src/Addition_Game.java
+++ src/Addition_Game.java
@@ -3,6 +3,9 @@ public class Addition_Game {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello class");
+		
+		// add an outline
+		// do some stuff
 	}
 
 }
